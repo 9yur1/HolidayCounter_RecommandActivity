1eaffadf16627919c93b6b3d62bd9aeb063eeb09
Kyuri Park <kyuri0924@khu.ac.kr>
2022-06-05 21:02:03 +0900

    Updated final files

---
+++ android/app/src/main/java/com/example/holidaycounter/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.holidaycounter;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Display;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    TextView todayDate;
+    TextView dateName;
+    TextView locDate;
+    TextView leftDate;
+    TextView title;
+    String url = "http://www.holiday-counter.tk:8080/app";
+    String msg;
+    final Bundle bundle = new Bundle();
+
+    int standardSize_X, standardSize_Y;
+    float density;
+
+    public void getStandardSize() {
+        Point ScreenSize = getScreenSize(this);
+        density  = getResources().getDisplayMetrics().density;
+
+        standardSize_X = (int) (ScreenSize.x / density);
+        standardSize_Y = (int) (ScreenSize.y / density);
+    }
+
+    public Point getScreenSize(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        return  size;
+    }
+
+
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String[] data = bundle.getString("message").split(",");
+            todayDate.setText(data[0]);
+            dateName.setText(data[1]);
+            locDate.setText(data[2].substring(0, 4) + "년 " + data[2].substring(4, 6) + "월 " + data[2].substring(6) + "일");
+            leftDate.setText("D-" + data[3]);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        todayDate = findViewById(R.id.todayDate);
+        dateName = findViewById(R.id.dateName);
+        locDate = findViewById(R.id.locDate);
+        leftDate = findViewById(R.id.leftDate);
+        title = findViewById(R.id.title);
+
+        new Thread() {
+            public void run() {
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect(url).get();
+                    Element elements = doc.select("body").first();
+                    msg = elements.text();
+                    bundle.putString("message", msg);
+                    Message msg = handler.obtainMessage();
+                    msg.setData(bundle);
+                    handler.sendMessage(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+}
---
+++ nodejs/server.js
@@ -0,0 +1,203 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const xml2js = require("xml2js");
+const static = require("serve-static");
+require("dotenv").config();
+
+// Modify the values as needed
+var year = "2022";
+var month = "09";
+var operation = "getRestDeInfo";
+
+// Do not modify the values
+var SERVEICE_KEY = process.env.API_KEY;
+var url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/";
+var queryParams = "?" + "solYear" + "=" + year;
+queryParams += "&" + "solMonth" + "=" + month;
+queryParams += "&" + "ServiceKey" + "=" + SERVEICE_KEY;
+let requestUrl = url + operation + queryParams;
+
+// Empty variables
+var text = "";
+var dateName = [];
+var locdate = [];
+var tempArr = [];
+
+// To run EJS engine
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(static(__dirname));
+
+// To get today date by using Date
+function getTodayDate() {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = ("0" + (today.getMonth() + 1)).slice(-2);
+  var day = ("0" + today.getDate()).slice(-2);
+  var dateString = year + "-" + month + "-" + day;
+
+  //return "2022-12-25"; // 테스트용 날짜를 입력하세요. *** 테스트가 끝나면 주석처리하고 커밋해주세요. *** ex) 2022-09-12, 2022-09-15, 2022-10-04
+  return dateString;
+}
+
+// To get modified date from locdate
+function getModifiedDate(locdate) {
+  return (
+    locdate.substr(0, 4) +
+    "-" +
+    locdate.substr(4, 2) +
+    "-" +
+    locdate.substr(6, 2)
+  );
+}
+
+// To get remaining days from locdate
+function getLeftDate(locdate) {
+  var today = new Date(getTodayDate()); // today date
+  var holiday = new Date(getModifiedDate(locdate)); // holiday date
+  var diffDate = today.getTime() - holiday.getTime();
+  var dday = Math.abs(diffDate / (1000 * 3600 * 24));
+  return dday;
+}
+
+// To change parameter
+function changeParams(year, month, operation) {
+  queryParams = "?" + "solYear" + "=" + year;
+  queryParams += "&" + "solMonth" + "=" + month;
+  queryParams += "&" + "ServiceKey" + "=" + SERVEICE_KEY;
+  requestUrl = url + operation + queryParams;
+}
+
+// To check is this left holiday
+function cmpDate(date) {
+  var today = getTodayDate().replace(/\-/g, "");
+  if (Number(today) <= Number(date)) {
+    return true;
+  }
+}
+
+// To get data from holiday api by using parmas
+function getData() {
+  request.get(requestUrl, (err, res, body) => {
+    if (err) {
+      console.log("err => " + err);
+    } else {
+      if (res.statusCode == 200) {
+        // Read url success
+        var result = body;
+        var parser = new xml2js.Parser();
+        parser.parseString(result, function (err, res) {
+          text = JSON.stringify(res);
+          // Get dataName method
+          dateName = [];
+          var idx = text.indexOf("dateName", 0);
+          while (idx != -1) {
+            var start = text.indexOf("[", idx) + 2;
+            var end = text.indexOf("]", idx) - 1;
+            var tempStr = text.substring(start, end);
+            dateName.push(tempStr);
+            idx = text.indexOf("dateName", idx + 1);
+          }
+          console.log(dateName);
+          // Get locdate method
+          locdate = [];
+          idx = text.indexOf("locdate", 0);
+          while (idx != -1) {
+            var start = text.indexOf("[", idx) + 2;
+            var end = text.indexOf("]", idx) - 1;
+            var tempStr = text.substring(start, end);
+            locdate.push(tempStr);
+            idx = text.indexOf("locdate", idx + 1);
+          }
+          console.log(locdate);
+          var holiArr = [];
+          for (var i = 0; i < dateName.length; i++) {
+            holiArr.push(getLeftDate(locdate[i]));
+          }
+          console.log(holiArr);
+          // Create tempArr to save dateName and locdate and leftDate at once
+          tempArr = [];
+          // To check left holiday
+          var check = false;
+          for (var i = 0; i < locdate.length; i++) {
+            if (cmpDate(locdate[i])) {
+              check = true;
+              tempArr.push(dateName[i]); // Get recent holiday name
+              tempArr.push(locdate[i]); // Get recent holiday date
+              tempArr.push(holiArr[i]); // Get leftDate through locdate
+              break;
+            }
+          }
+          console.log(tempArr);
+          if (!check) {
+            // If there are no holidays left this month
+            console.log(
+              "이번 달에는 남은 공휴일이 없습니다. 다음달 데이터를 불러옵니다."
+            );
+            if (Number(month) < 12) {
+              // Get next month data
+              month = String(Number(month) + 1).padStart(2, "0");
+            } else {
+              // Get next year data
+              year = String(Number(year) + 1).padStart(4, "0");
+              month = "01";
+            }
+            changeParams(year, month, operation);
+            getData();
+          }
+          console.log("* api로부터 데이터를 불러왔습니다.");
+          console.log("오늘의 날짜는", getTodayDate(), "입니다.");
+          console.log(
+            "가장 가까운 공휴일인 [" +
+              tempArr[0] +
+              "]의 날짜는 [" +
+              tempArr[1] +
+              "]이고, [" +
+              tempArr[2] +
+              "]일 남았습니다."
+          );
+        });
+      }
+    }
+  });
+}
+
+// To initialize datas when calling webpage
+function init() {
+  // Intialize Year, Month
+  var date = getTodayDate().split("-");
+  year = date[0];
+  month = date[1];
+  operation = "getRestDeInfo";
+  changeParams(year, month, operation);
+  // Get data from holiday api
+  getData();
+}
+
+// Get request for web service
+app.get("/", function (req, res) {
+  init();
+  // Send data from nodejs to ejs
+  res.render("data.ejs", { data: tempArr }, function (err, html) {
+    if (err) {
+      console.log(err);
+    }
+    res.end(html); // End response
+  });
+  // send data.ejs
+  res.sendFile(__dirname + "/views/data.ejs");
+});
+
+// Get request for app service(Send main data)
+app.get("/app", function (req, res) {
+  init();
+  res.send(
+    getTodayDate() + "," + tempArr[0] + "," + tempArr[1] + "," + tempArr[2]
+  );
+});
+
+init();
+
+const port = 8080;
+app.listen(port, () => console.log("Listening on port " + port));
